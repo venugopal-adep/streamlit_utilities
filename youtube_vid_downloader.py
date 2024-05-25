4235c71a5071d976fb9c8fe3054fd5a624b4c8c5
venugopal-adep <92349362+venugopal-adep@users.noreply.github.com>
2024-05-25 14:22:18 +0530

    Update youtube_vid_downloader.py

--- youtube_vid_downloader.py
+++ youtube_vid_downloader.py
@@ -9,32 +9,32 @@ url = st.text_input('Enter the URL of the YouTube video you wish to download:')
 
 if url:
     yt = YouTube(url)
-    st.write(f"Video Title: {yt.title}")
+    st.write(f"*Video Title:* {yt.title}")
     st.image(yt.thumbnail_url)
 
     stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
     options = [(i.itag, i.resolution) for i in stream]
     selected_option = st.selectbox('Choose the quality/resolution of the video to download:', options, format_func=lambda x: x[1])
 
-#if st.button('Download Video'):
-    selected_stream = stream.get_by_itag(selected_option[0])
-    safe_filename = yt.title.replace('/', '-').replace('\\', '-').replace(':', '-').replace('|', '-').replace('*', '-').replace('?', '-').replace('"', '-').replace('<', '-').replace('>', '-')
+    if st.button('Download Video'):
+        selected_stream = stream.get_by_itag(selected_option[0])
+        safe_filename = yt.title.replace('/', '-').replace('\\', '-').replace(':', '-').replace('|', '-').replace('*', '-').replace('?', '-').replace('"', '-').replace('<', '-').replace('>', '-')
 
-    with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
-        try:
+        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+            try:
                 # Download the video directly to a temporary file
-            selected_stream.download(output_path=os.path.dirname(tmpfile.name), filename=safe_filename)
-            tmpfile.close()
+                selected_stream.download(output_path=os.path.dirname(tmpfile.name), filename=safe_filename)
+                tmpfile.close()
 
                 # Provide a download button
-            with open(tmpfile.name, 'rb') as f:
-                st.download_button(label='Download Video',
-                               data=f,
-                               file_name=safe_filename + ".mp4",
-                               mime="video/mp4",on_click=False)
-        except Exception as e:
-            st.error(f"Failed to download the video: {e}")
-        finally:
+                with open(tmpfile.name, 'rb') as f:
+                    st.download_button(label="Download Video",
+                                       data=f,
+                                       file_name=safe_filename + ".mp4",
+                                       mime="video/mp4")
+            except Exception as e:
+                st.error(f"Failed to download the video: {e}")
+            finally:
                 # Ensure the temporary file is deleted after serving
-            if os.path.exists(tmpfile.name):
-                os.remove(tmpfile.name)
+                if os.path.exists(tmpfile.name):
+                    os.remove(tmpfile.name)
